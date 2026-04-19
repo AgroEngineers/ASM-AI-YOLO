3b8aeb1504da9b68a6a927e17b906057d75430ca
Robo_Start <craft.surkov.roma@gmail.com>
2026-04-19 18:28:57 +0300

    YOLO AI module

--- yolo.py
+++ yolo.py
@@ -1,30 +1,65 @@
 from pathlib import Path
 from typing import Union
 
+import torch
+from ultralytics import YOLO
+
 import numpy
 from asm.api.ai import ASMAI, AIResult, AIExpansion
-from asm.api.base import ModuleInformation, ModuleTask, ModuleTaskInput, ModuleTaskOutput, ModuleConfiguration, \
-    ContainerParameterResults
+from asm.api.base import ModuleTask, ModuleTaskInput, ModuleTaskOutput, ModuleConfiguration, ModuleInformation, \
+    ContainerParameterResults, ModuleRequirement
 
 
-class TFLite(ASMAI):
+class YOLOai(ASMAI):
+    model = None
+    current_labels = None
+    name = ""
+
     def expansions(self) -> AIExpansion:
-        pass
+        return AIExpansion(["pt", "onnx"])
 
-    def available_labels(self, labels: Path) -> list[str]:
-        pass
+    def available_labels(self) -> list[str]:
+        return self.current_labels
 
-    def process(self, frame: numpy.ndarray) -> tuple[AIResult, Union[list[ContainerParameterResults], None]]:
-        pass
+    def process(self, frame: numpy.ndarray) -> tuple[
+        Union[AIResult, None], Union[list[ContainerParameterResults], None]]:
+        if self.model is None:
+            raise ModuleNotFoundError()
 
-    def load(self, model: Path, labels: Path) -> bool:
-        pass
+        results = self.model.predict(source=frame, conf=0.25, verbose=False)
+
+        label = self.current_labels[int(results[0].boxes.cls[0].item())]
+
+        return AIResult(self.name, label), None
+
+    def load(self, model_path: Path, labels_path: Union[Path, None]) -> bool:
+        self.name = model_path.name
+        try:
+            self.model = YOLO(str(model_path))
+            self.current_labels = list(self.model.names.values())
+
+            return True
+        except Exception as e:
+            return False
 
     def unload(self) -> None:
-        pass
+        if self.model is not None:
+            self.model = None
+            self.current_labels = None
+
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+                torch.cuda.ipc_collect()
+
 
     def module_info(self) -> ModuleInformation:
-        pass
+        return ModuleInformation(
+            name="YOLO",
+            version="1.0",
+            requirements=[
+                ModuleRequirement("torch"), ModuleRequirement("ultralytics")
+            ]
+        )
 
     def configuration(self, configuration: ModuleConfiguration):
         pass
